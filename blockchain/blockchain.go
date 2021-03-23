bbce388304dcc5ed2157e6b7f167bc22c64641ba
hongjinui <36667004+hongjinui@users.noreply.github.com>
2021-03-23 12:37:49 +0900

    part2 commit

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,20 +1,20 @@
 package blockchain
 
-type Blockchain struct {
+type Blockchain struct { // blockchain struct
 	blocks []*Block
 }
 
-func (bc *Blockchain) AddBlock(data string) {
+func (bc *Blockchain) AddBlock(data string) { // 블록체인에 블록 추가
 	prevBlock := bc.blocks[len(bc.blocks)-1]
 	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
 
 }
 
-func NewBlockchain() *Blockchain {
+func NewBlockchain() *Blockchain { // 새로운 블록체인 생성
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
 
-func (bc *Blockchain) GetBlocks() []*Block {
+func (bc *Blockchain) GetBlocks() []*Block { // 블록체인에 블록들 가져오기
 	return bc.blocks
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/hongjinui/go-blockchain/blockchain"
 )
@@ -20,6 +21,9 @@ func main() {
 		fmt.Printf("Prev : hash : %x\n", block.PrevBlockHash)
 		fmt.Printf("Data : %s\n", block.Data)
 		fmt.Printf("Hash : %x\n", block.Hash)
+
+		pow := blockchain.NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validation()))
 		fmt.Println()
 	}
 }
